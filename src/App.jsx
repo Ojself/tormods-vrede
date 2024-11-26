072b8378a20c5e352a32dae0dd80f2a6ede6215d
Tormod <tormod.flesjo@gmail.com>
2024-11-26 12:34:16 +0100

    quick commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import tormod from '/tormod.webp';
 import fire from '/fire.gif';
 import logo from '/logo.png';
@@ -6,12 +6,29 @@ import './App.css';
 import ImageGalleryModal from "./ImageGalleryModal";
 import NutritionModal from "./NutritionModal";
 
+const ignoreTouch = (e) => {
+  e.preventDefault();
+}
+
 
 const firePositions = [-40, -21, -2, 17, 36, 54]; // Define left positions
 
 function App() {
   const [showImageGallery, setShowImageGallery] = useState(false);
   const [showNutrition, setShowNutrition] = useState(false);
+
+
+  useEffect(() => {
+    document.addEventListener('gesturestart', ignoreTouch);
+    document.addEventListener('gesturechange', ignoreTouch);
+    document.addEventListener('gestureend', ignoreTouch);
+    return () => {
+    document.removeEventListener('gesturestart', ignoreTouch);
+    document.removeEventListener('gesturechange', ignoreTouch);
+    document.removeEventListener('gestureend', ignoreTouch);
+    };
+  }, []);
+
 
   return (
     <div className="wrapper">
